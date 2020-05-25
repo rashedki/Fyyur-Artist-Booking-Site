44daa079038181b482b8eeb1962f75f466783044
Khalil Al-Rashid <rashedki@hotmail.com>
2020-05-25 14:27:14 +0300

    Correcting the indent

--- app.py
+++ app.py
@@ -467,7 +467,7 @@ def create_shows():
 def create_show_submission():
   # called to create new shows in the db, upon submitting new show listing form
   # TODO: insert form data as a new Show record in the db, instead
-try:
+    try:
       art_id = request.form.get('artist_id')
       ven_id = request.form.get('venue_id')
       strt_time = request.form.get("start_time")
@@ -476,19 +476,19 @@ try:
       db.session.commit()
       # on successful db insert, flash success
       flash('Show was successfully listed!')
-  except:
+    except:
       db.session.rollback()
       print(sys.exc_info())
       flash(sys.exc_info())
-  finally:
+    finally:
       db.session.close()
 
   # on successful db insert, flash success
-  flash('Show was successfully listed!')
+      flash('Show was successfully listed!')
   # TODO: on unsuccessful db insert, flash an error instead.
   # e.g., flash('An error occurred. Show could not be listed.')
   # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
-  return render_template('pages/home.html')
+      return render_template('pages/home.html')
 
 @app.errorhandler(404)
 def not_found_error(error):
