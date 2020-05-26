cf47815631e0f978f5ca79f49d395472b4396170
Khalil Al-Rashid <rashedki@hotmail.com>
2020-05-26 19:31:46 +0300

    Correcting the Venues to read from the database

--- app.py
+++ app.py
@@ -56,28 +56,51 @@ def index():
 def venues():
   # TODO: replace with real venues data.
   #       num_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
-    "city": "San Francisco",
-    "state": "CA",
-    "venues": [{
-      "id": 1,
-      "name": "The Musical Hop",
-      "num_upcoming_shows": 0,
-    }, {
-      "id": 3,
-      "name": "Park Square Live Music & Coffee",
-      "num_upcoming_shows": 1,
-    }]
-  }, {
-    "city": "New York",
-    "state": "NY",
-    "venues": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }]
-  return render_template('pages/venues.html', areas=data);
+  # data=[{
+  #   "city": "San Francisco",
+  #   "state": "CA",
+  #   "venues": [{
+  #     "id": 1,
+  #     "name": "The Musical Hop",
+  #     "num_upcoming_shows": 0,
+  #   }, {
+  #     "id": 3,
+  #     "name": "Park Square Live Music & Coffee",
+  #     "num_upcoming_shows": 1,
+  #   }]
+  # }, {
+  #   "city": "New York",
+  #   "state": "NY",
+  #   "venues": [{
+  #     "id": 2,
+  #     "name": "The Dueling Pianos Bar",
+  #     "num_upcoming_shows": 0,
+  #   }]
+  # }]
+    data =[]
+    city_to_state = {}
+    for states in Venue.query.distinct(Venue.city, Venue.state).all():
+        for val in Venue.query.filter(Venue.city == states.city, Venue.state == states.state):
+          if(val.city in city_to_state.keys() and city_to_state[val.city] == val.state):
+            for x in data:
+              if ((x.get("state") == val.state) and (x.get("city") == val.city)):
+                x.get("venues").append({
+                  "id": val.id,
+                  "name": val.name,
+                  "num_upcoming_shows": Show.query.filter(val.id == Show.venue_id, Show.start_time > datetime.now()).count()
+                })
+          else:
+            city_to_state[val.city] = val.state
+            data.append({
+                "city": val.city,
+                "state": val.state,
+                "venues":[{
+                     "id": val.id,
+                     "name": val.name,
+                     "num_upcoming_shows": Show.query.filter(val.id == Show.venue_id, Show.start_time > datetime.now()).count()
+                }]
+             })
+    return render_template('pages/venues.html', areas=data);
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
